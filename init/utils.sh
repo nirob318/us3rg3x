9bb816b6aeac02978bdb4432534004de04c379c6
Nirob Ahmed <65337860+nirob318@users.noreply.github.com>
2022-04-02 03:18:26 +0000

    Update utils.sh

--- init/utils.sh
+++ init/utils.sh
@@ -64,27 +64,6 @@ addHeroku() {
     git remote add heroku $HEROKU_GIT_URL
 }
 
-addUpstream() {
-    git remote add $UPSTREAM_REMOTE ${UPSTREAM_REPO%.git}.git
-}
-
-updateUpstream() {
-    git remote rm $UPSTREAM_REMOTE && addUpstream
-}
-
-fetchUpstream() {
-    git fetch $UPSTREAM_REMOTE &> /dev/null
-}
-
-fetchBranches() {
-    local r_bs l_bs
-    r_bs=$(grep -oP '(?<=refs/heads/)\w+' < <(git ls-remote --heads $UPSTREAM_REMOTE))
-    l_bs=$(grep -oP '\w+' < <(git branch))
-    for r_b in $r_bs; do
-        [[ $l_bs =~ $r_b ]] || git branch $r_b $UPSTREAM_REMOTE/$r_b &> /dev/null
-    done
-}
-
 updateBuffer() {
     git config http.postBuffer 524288000
 }
